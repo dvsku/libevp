b436dc1a312cd5d71f7bd295066fced2e0cd7676
dvsku <nikola.stojsin@hotmail.com>
2022-12-07 10:03:16 +0100

    moved definitions to libevp.h

--- src/libevp.h
+++ src/libevp.h
@@ -3,17 +3,32 @@
 #ifndef LIBEVP_H
 #define LIBEVP_H
 
+#include <vector>
 #include <string>
 #include <functional>
 
-#include "libevp_definitions.h"
+#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+#include <experimental/filesystem>
+
+namespace filesys = std::experimental::filesystem;
+
+constexpr char UNKNOWN_HEADER_BYTES[60] {
+	53,50,53,99,49,55,97,54,97,55,99,102,98,99,100,55,53,52,49,50,101,99,100,48,54,57,100,52,
+	98,55,50,99,51,56,57,0,16,0,0,0,78,79,82,77,65,76,95,80,65,67,75,95,84,89,80,69,100,0,0,0
+};
+
+constexpr char RESERVED_BYTES[16] {};
+
+constexpr uint32_t DATA_START_OFFSET = 76;
+constexpr uint32_t HEADER_END_OFFSET = 60;
+constexpr uint32_t OFFSET_BETWEEN_NAMES = 36;
 
 namespace dvsku {
 	class evp {
 		protected:
 			struct file_desc {
 				std::string m_path;
-				BUF8 m_data;
+				std::vector<uint8_t> m_data;
 				unsigned char m_data_hash[16];
 				size_t m_data_size;
 				size_t m_path_size;

2ebfe54d2c73a18d49ec7db1537a247c2ef5baf2
dvsku <nikola.stojsin@hotmail.com>
2023-08-30 02:04:26 +0200

    Update libdvsku_crypt

--- src/lib/libdvsku_crypt/libdvsku_crypt.h
+++ src/lib/libdvsku_crypt/libdvsku_crypt.h
@@ -1,154 +1,113 @@
 #pragma once
 
-#ifndef LIBDVSKU_CRYPT
-#define LIBDVSKU_CRYPT
-
-#define WIN32_LEAN_AND_MEAN
-
-#include <cstdint>
 #include <string>
 #include <vector>
 #include <functional>
+#include <filesystem>
 
-#ifndef _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
-#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
-#endif
+#include "utilities/dv_result.hpp"
 
-#include <experimental/filesystem>
+namespace libdvsku::crypt {
+	enum class file_filter : unsigned char {
+		none,			// include all files
+		client_only,	// include only Talisman Online client files
+		server_only		// include only Talisman Online server files
+	};
 
-namespace std_filesys = std::experimental::filesystem;
-
-#include "utilities/utilities_filesys.h"
-
-#define CRYPT_OK				0x00
-#define CRYPT_INPUT_ERROR		0x01
-#define CRYPT_OUTPUT_ERROR		0x02
-#define CRYPT_ERROR				0x03
-#define CRYPT_NOT_ENCRYPTED		0x04
-#define CRYPT_ENCRYPTED			0x05
-#define CRYPT_CANCELLED			0x06
-
-// unsigned char vector buffer
-#define BUFV8 std::vector<uint8_t>
-
-// unsigned char vector buffer reference
-#define BUFV8_REF std::vector<uint8_t>&
-
-// const unsigned char vector buffer reference
-#define BUFV8_REF_C const std::vector<uint8_t>&
-
-// unsigned char pointer
-#define BUF8 uint8_t*
-
-#define KEY const char*
-
-typedef uint32_t crypt_result;
-
-namespace dvsku::crypt {
 	class libdvsku_crypt {
-		protected:
-			struct box {
-				uint32_t m_index_A = 0;
-				uint32_t m_index_B = 0;
-				uint64_t m_previous_offset = 0;
-				uint8_t m_data[256] = {};
-			};
-
-			std::shared_ptr<box> m_box;
-			std::string m_key;
-
-			libdvsku_crypt();
-
 		public:
 			// void f()
 			typedef std::function<void()> notify_start;
 
 			// void f(crypt_result)
-			typedef std::function<void(crypt_result)> notify_finish;
+			typedef std::function<void(dv_result)> notify_finish;
 
 			// void f(float)
 			typedef std::function<void(float)> notify_update;
 
 			// void f(crypt_result)
-			typedef std::function<void(crypt_result)> notify_error;
+			typedef std::function<void(dv_result)> notify_error;
+
+			typedef std::filesystem::path	FILE_PATH;
+			typedef std::filesystem::path	FOLDER_PATH;
+			typedef std::vector<uint8_t>	BUFFER;
 
 		public:
-			libdvsku_crypt(KEY key);
-			virtual ~libdvsku_crypt();
+			libdvsku_crypt(const char* key);
 
 			// Set encryption/decryption key
-			void set_key(KEY key);
+			void set_key(const char* key);
 
 			// Check if file is encrypted by checking magic
-			bool is_file_encrypted(FILE_PATH_REF_C file);
+			bool is_file_encrypted(const FILE_PATH& file);
+
+			// Check if buffer is encrypted by checking magic
+			bool is_buffer_encrypted(uint8_t* buffer_ptr, size_t size);
 
 			// Preforms encryption on the input file and saves the encrypted data to
 			// the output file.
 			// Encryption is skipped if file is encrypted.
 			// If output is empty, result will be saved to input.
-			crypt_result encrypt_file(FILE_PATH_REF_C input, FILE_PATH_REF_C output = "");
+			dv_result encrypt_file(const FILE_PATH& input, const FILE_PATH& output = "");
 			
 			// Preforms encryption on the input file and saves the encrypted data to
 			// the out buffer.
 			// Encryption is skipped if file is encrypted.
-			crypt_result encrypt_file(FILE_PATH_REF_C input, BUFV8_REF out);
+			dv_result encrypt_file(const FILE_PATH& input, BUFFER& out);
 
 			// Preforms encryption on every file inside folder and subfolders and
 			// saves every encrypted file to the output folder with the same folder structure.
 			// Encryption is skipped if file is encrypted.
 			// If output is empty, result will be saved to input.
-			crypt_result encrypt_folder(FOLDER_PATH_REF_C input, FOLDER_PATH_REF_C output = "",
-				FILE_FILTER_REF_C filter = FILE_FILTER_NONE);
+			dv_result encrypt_folder(const FOLDER_PATH& input, const FOLDER_PATH& output = "",
+				file_filter filter = file_filter::none);
 
 			// Preforms encryption asynchronously on every file inside folder and subfolders and
 			// saves every encrypted file to the output folder with the same folder structure.
 			// Encryption is skipped if file is encrypted.
 			// If output is empty, result will be saved to input.
-			void encrypt_folder_async(FOLDER_PATH_REF_C input, FOLDER_PATH_REF_C output = "", 
-				FILE_FILTER_REF_C filter = FILE_FILTER_NONE, bool* cancel = nullptr, notify_start started = nullptr, 
+			void encrypt_folder_async(const FOLDER_PATH& input, const FOLDER_PATH& output = "",
+				file_filter filter = file_filter::none, bool* cancel = nullptr, notify_start started = nullptr,
 				notify_update update = nullptr, notify_finish finished = nullptr, notify_error error = nullptr);
 
 			// Preforms encryption on the buffer.
 			// Encryption is skipped if file is encrypted.
 			// Buffer content and size will be modified.
-			crypt_result encrypt_buffer(BUFV8_REF buffer);
+			dv_result encrypt_buffer(BUFFER& buffer);
 
 			// Preforms decryption on the input file and saves the data to the output file.
 			// Decryption is skipped if file is not encrypted.
 			// If output is empty, result will be saved to input.
-			crypt_result decrypt_file(FILE_PATH_REF_C input, FILE_PATH_REF_C output = "");
+			dv_result decrypt_file(const FILE_PATH& input, const FILE_PATH& output = "");
 			
 			// Preforms decryption on the input file and saves the data to the out buffer.
 			// Decryption is skipped if file is not encrypted.
-			crypt_result decrypt_file(FILE_PATH_REF_C input, BUFV8_REF out);
+			dv_result decrypt_file(const FILE_PATH& input, BUFFER& out);
 
 			// Preforms decryption on every file inside folder and subfolders and
 			// saves every decrypted file to the output folder with the same folder structure.
 			// Decryption is skipped if file is not encrypted.
 			// If output is empty, result will be saved to input.
-			crypt_result decrypt_folder(FOLDER_PATH_REF_C input, FOLDER_PATH_REF_C output = "");
+			dv_result decrypt_folder(const FOLDER_PATH& input, const FOLDER_PATH& output = "");
 
 			// Preforms decryption asynchronously on every file inside folder and subfolders and
 			// saves every decrypted file to the output folder with the same folder structure.
 			// Decryption is skipped if file is not encrypted.
 			// If output is empty, result will be saved to input.
-			void decrypt_folder_async(FOLDER_PATH_REF_C input, FOLDER_PATH_REF_C output = "",
+			void decrypt_folder_async(const FOLDER_PATH& input, const FOLDER_PATH& output = "",
 				bool* cancel = nullptr, notify_start started = nullptr, notify_update update = nullptr,
 				notify_finish finished = nullptr, notify_error error = nullptr);
-
-			// Check if buffer is encrypted by checking magic
-			bool is_buffer_encrypted(BUF8 buffer, uint64_t size);
 
 			// Preforms decryption then decompression on the buffer.
 			// Buffer content and size will be modified.
 			// Decryption is skipped if file is not encrypted.
-			crypt_result decrypt_buffer(BUFV8_REF buffer);
+			dv_result decrypt_buffer(BUFFER& buffer);
 
 			// Preforms decryption on the buffer. Buffer data is replaced with decrypted data
 			// if decryption was successful. 
 			// Decryption is skipped if file is not encrypted.
 			// Magic is replaced with zeroes and new_size contains the new buffer size.
-			crypt_result decrpyt_buffer(BUF8 buffer, uint64_t size, uint64_t* new_size);
+			dv_result decrpyt_buffer(uint8_t* buffer_ptr, size_t size, size_t* new_size);
 
 			// Remark: Assummes the buffer is encrypted! Only use on encrypted buffers!
 			// Preforms decryption on the buffer. Buffer data is replaced with decrypted data
@@ -157,41 +116,23 @@ namespace dvsku::crypt {
 			// Use when a large buffer is split into multiple smaller. 
 			// Offset is offset from start of first buffer.
 			// Call decrpyt_split_buffer_cleanup() after you finish decrypting.
-			crypt_result decrpyt_split_buffer(BUF8 buffer, uint64_t size, uint64_t offset);
+			dv_result decrpyt_split_buffer(uint8_t* buffer_ptr, size_t size, size_t offset);
 
 			// Cleanup box for current split buffer decryption
 			void decrpyt_split_buffer_cleanup();
 
-		protected:
-			dvsku::crypt::libdvsku_crypt::box generate_box();
-			
-			// Preforms encryption/decryption on the buffer. Buffer data is replaced with encrypted data
-			// if encryption was successful.
-			crypt_result crypt(BUFV8_REF buffer);
+		private:
+			std::string m_key;
+
+		private:
+			libdvsku_crypt();
 
 			// Preforms encryption/decryption on the buffer. Buffer data is replaced with encrypted data
 			// if encryption was successful.
-			crypt_result crypt(BUF8 buffer, uint64_t size, uint64_t offset = 0, bool keep_box = false);
+			dv_result crypt(BUFFER& buffer);
 
-			// Check if buffer has magic
-			bool has_magic(BUFV8_REF_C buffer);
-
-			// Check if buffer has magic
-			bool has_magic(BUF8 buffer, uint64_t size);
-
-			// Writes magic to buffer. Buffer size is modified to fit the magic.
-			void write_magic(BUFV8_REF buffer);
-
-			// Writes magic to buffer. Buffer must have space available for it or it will overwrite existing data.
-			void write_magic(BUF8 buffer, uint64_t size);
-
-			// Removes magic from buffer. Buffer size is modified when magic is removed.
-			void remove_magic(BUFV8_REF buffer);
-
-			// Removes magic from buffer. Magic data is set to zeroes.
-			// User must handle the new size (smaller by 4 bytes).
-			void remove_magic(BUF8 buffer, uint64_t size);
+			// Preforms encryption/decryption on the buffer. Buffer data is replaced with encrypted data
+			// if encryption was successful.
+			dv_result crypt(uint8_t* buffer_ptr, size_t size, size_t offset = 0, bool keep_box = false);
 	};
 }
-
-#endif
---
+++ src/lib/libdvsku_crypt/utilities/dv_result.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+
+namespace libdvsku {
+	enum class dv_status : unsigned char {
+		ok			= 0x0,
+		error		= 0x1,
+		cancelled	= 0x2
+	};
+
+	struct dv_result {
+		dv_status status	= dv_status::ok;
+		std::string msg		= "";
+
+		dv_result() {};
+		dv_result(dv_status _status) : status(_status) {};
+		dv_result(dv_status _status, const std::string& _msg) : status(_status), msg(_msg) {};
+
+		bool operator!() const {
+			return status != dv_status::ok;
+		}
+
+		explicit operator bool() const {
+			return status == dv_status::ok;
+		}
+	};
+}

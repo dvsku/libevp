cad90953be0a20033b7ce4198650efb6264b1006
dvsku <nikola.stojsin@hotmail.com>
2023-09-20 02:58:59 +0200

    Unpack file to stringstream tests

--- test/v1/test_unpacking.cpp
+++ test/v1/test_unpacking.cpp
@@ -191,6 +191,46 @@ void v1_get_file_from_evp_encrypted(const std::string& base) {
 	ASSERT(compare_buffers(buffer, contents));
 }
 
+void v1_get_file_from_evp_stream(const std::string& base) {
+	std::string input = base + std::string("/test/v1/resources/valid_folders.evp");
+	std::string valid = base + std::string("/test/v1/resources/files_to_pack/text_1.txt");
+
+	std::stringstream ss;
+
+	auto r1 = evp::get_file_from_evp(input, "text_1.txt", ss);
+	ASSERT(r1);
+
+	std::ifstream stream(valid, std::ios::in | std::ios::binary);
+
+	ASSERT(stream.is_open());
+	ASSERT(!stream.bad());
+
+	std::vector<uint8_t> buffer((std::istreambuf_iterator<char>(ss)), std::istreambuf_iterator<char>());
+	std::vector<uint8_t> contents((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+
+	ASSERT(compare_buffers(buffer, contents));
+}
+
+void v1_get_file_from_evp_stream_encrypted(const std::string& base) {
+	std::string input = base + std::string("/test/v1/resources/valid_folders_encrypted.evp");
+	std::string valid = base + std::string("/test/v1/resources/files_to_pack/text_1.txt");
+
+	std::stringstream ss;
+
+	auto r1 = evp::get_file_from_evp(input, "text_1.txt", ss, true, "dvsku");
+	ASSERT(r1);
+
+	std::ifstream stream(valid, std::ios::in | std::ios::binary);
+
+	ASSERT(stream.is_open());
+	ASSERT(!stream.bad());
+
+	std::vector<uint8_t> buffer((std::istreambuf_iterator<char>(ss)), std::istreambuf_iterator<char>());
+	std::vector<uint8_t> contents((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+
+	ASSERT(compare_buffers(buffer, contents));
+}
+
 int main(int argc, char* argv[]) {
 	if (argc == 3) {
 		switch (std::stoi(argv[1])) {
@@ -200,6 +240,8 @@ int main(int argc, char* argv[]) {
 			case 3: v1_unpacking_folder_encrypted(argv[2]);				break;
 			case 4: v1_get_file_from_evp(argv[2]);						break;
 			case 5: v1_get_file_from_evp_encrypted(argv[2]);			break;
+			case 6: v1_get_file_from_evp_stream(argv[2]);				break;
+			case 7: v1_get_file_from_evp_stream_encrypted(argv[2]);		break;
 			default: break;
 		}
 	}

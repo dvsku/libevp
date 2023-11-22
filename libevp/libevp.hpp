d88a74980d0547e256ec7ad433184e7038569614
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-22 21:03:07 +0100

    Explicitly define file_filter values

--- libevp/libevp.hpp
+++ libevp/libevp.hpp
@@ -16,10 +16,10 @@
 #endif
 
 namespace libevp {
-    enum class file_filter {
-        none,           // include all files
-        client_only,    // include only Talisman Online client files
-        server_only     // include only Talisman Online server files
+    enum class file_filter : unsigned int {
+        none        = 0x00,   // include all files
+        client_only = 0x01,   // include only Talisman Online client files
+        server_only = 0x02    // include only Talisman Online server files
     };
 
     struct evp_result {

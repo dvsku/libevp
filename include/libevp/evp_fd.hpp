16c9d8a7ab6b9bf8739f09e6c2320decda239521
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-08-18 02:23:18 +0200

    Add hash to evp_fd
    - hash is MD5

--- include/libevp/evp_fd.hpp
+++ include/libevp/evp_fd.hpp
@@ -2,11 +2,13 @@
 
 #include <cstdint>
 #include <string>
+#include <array>
 
 namespace libevp {
     struct evp_fd {
-        std::string file        = "";
-        uint32_t    data_offset = 0U;
-        uint32_t    data_size   = 0U;
+        std::string             file        = "";
+        uint32_t                data_offset = 0U;
+        uint32_t                data_size   = 0U;
+        std::array<uint8_t, 16> hash        = {};
     };
 }

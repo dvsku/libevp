24fceb9bded4445d2825daa77449dc337323bc48
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-08-19 14:39:10 +0200

    Update tests
    - add validate files test

--- tests/v1/test_misc.cpp
+++ tests/v1/test_misc.cpp
@@ -17,3 +17,11 @@ TEST(misc, get_files) {
     EXPECT_TRUE(files[2].file == "subfolder_2/text_3.txt");
     EXPECT_TRUE(files[3].file == "text_1.txt");
 }
+
+TEST(misc, validate_files) {
+    evp         evp;
+    std::string input = BASE_PATH + std::string("/tests/v1/resources/multiple_files.evp");
+
+    auto result = evp.validate_files(input);
+    ASSERT_TRUE(result);
+}

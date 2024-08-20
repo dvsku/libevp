5b023275eb9e3b0b9a2f7b708ca0e3a3a723a6de
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-08-20 23:50:22 +0200

    Update defs
    - rename EVP_BUFFER_SIZE to EVP_READ_CHUNK_SIZE
    - add buffer_t typename

--- source/libevp/defs.hpp
+++ source/libevp/defs.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <vector>
+#include <cstdint>
+
 namespace libevp {
-    constexpr uint32_t EVP_BUFFER_SIZE = 32 * 1024;
+    constexpr uint32_t EVP_READ_CHUNK_SIZE = 16 * 1024;
+
+    using buffer_t = std::vector<uint8_t>;
 }

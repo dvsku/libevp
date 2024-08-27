e88a96542f27db0955a53bc2074731499bb23091
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-08-27 02:09:00 +0200

    Clean up filtering

--- source/libevp/utilities/filtering.cpp
+++ source/libevp/utilities/filtering.cpp
@@ -3,13 +3,16 @@
 
 using namespace libevp;
 
-// Folders to include when filtering for client_only
-const std::vector<std::string> CLIENT_FOLDERS {
+////////////////////////////////////////////////////////////////////////////////
+// INTERNAL
+
+// Directories to include when filtering for client_only
+const std::vector<std::string> CLIENT_DIRS {
     "local", "maps", "model", "model2", "script", "ui", "audio", "music", "scene"
 };
 
-// Folders to include when filtering for server_only
-const std::vector<std::string> SERVER_FOLDERS {
+// Directories to include when filtering for server_only
+const std::vector<std::string> SERVER_DIRS {
     "local", "maps", "script"
 };
 
@@ -27,21 +30,24 @@ static bool path_contains_dir(const std::filesystem::path& path, const std::vect
 static bool path_contains_filename(const std::filesystem::path& path, const std::vector<std::string>& files);
 static bool path_contains_extension(const std::filesystem::path& path, const std::vector<std::string>& extensions);
 
-std::vector<std::filesystem::path> filtering::get_filtered_paths(const std::filesystem::path& dir, evp_filter filter) {
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC
+
+std::vector<FILE_PATH> filtering::get_filtered_paths(const DIR_PATH& input, evp_filter filter) {
     std::vector<std::filesystem::path> results;
 
-    for (auto const& dir_entry : std::filesystem::recursive_directory_iterator(dir)) {
+    for (auto const& dir_entry : std::filesystem::recursive_directory_iterator(input)) {
         if (!std::filesystem::is_regular_file(dir_entry.path())) continue;
 
         switch (filter) {
             case evp_filter::client_only: {
-                if (path_contains_dir(dir_entry.path(), CLIENT_FOLDERS) || path_contains_filename(dir_entry.path(), CLIENT_FILES))
+                if (path_contains_dir(dir_entry.path(), CLIENT_DIRS) || path_contains_filename(dir_entry.path(), CLIENT_FILES))
                     results.push_back(dir_entry.path());
 
                 break;
             }
             case evp_filter::server_only: {
-                if (path_contains_dir(dir_entry.path(), SERVER_FOLDERS) || path_contains_filename(dir_entry.path(), SERVER_FILES))
+                if (path_contains_dir(dir_entry.path(), SERVER_DIRS) || path_contains_filename(dir_entry.path(), SERVER_FILES))
                     results.push_back(dir_entry.path());
 
                 break;
@@ -52,6 +58,9 @@ std::vector<std::filesystem::path> filtering::get_filtered_paths(const std::file
 
     return results;
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// INTERNAL
 
 bool path_contains_dir(const std::filesystem::path& path, const std::vector<std::string>& dirs) {
     const std::string dir = path.parent_path().generic_string();
--- source/libevp/utilities/filtering.hpp
+++ source/libevp/utilities/filtering.hpp
@@ -1,14 +1,13 @@
 #pragma once
 
+#include "libevp/evp_defs.hpp"
+#include "libevp/model/evp_filter.hpp"
+
 #include <vector>
-#include <filesystem>
 
-namespace libevp {
-    enum class evp_filter : uint32_t;
-
-    class filtering {
-    public:
-        // Get paths to files to pack with filtering
-        static std::vector<std::filesystem::path> get_filtered_paths(const std::filesystem::path& dir, evp_filter filter);
-    };
+namespace libevp::filtering {
+    /*
+        Get filtered input files.
+    */
+    std::vector<FILE_PATH> get_filtered_paths(const DIR_PATH& input, evp_filter filter);
 }
